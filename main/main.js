784e579fb3ad91d69fc83f6dbf131121adbaba99
admin <school@thoughtworks.com>
2018-08-14 04:16:19 +0000

    first commit

---
+++ main/main.js
@@ -0,0 +1,18 @@
+module.exports = function main() {
+    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
+    console.log(`o) 最小值 = ${sequence.minimum()}
+// Write your code here
+`);
+};
+
+class Sequence {
+  constructor(input) {
+    // Write your code here
+  }
+
+  minimum() {
+    // Write your code here
+  }
+
+  // Write your code here
+}
