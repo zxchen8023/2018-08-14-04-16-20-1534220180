6d635b6cd486356a5a28b3d44b9db14bf9ec34b7
zxchen8023 <43513652+zxchen8023@users.noreply.github.com>
2018-09-24 19:07:43 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,7 +1,10 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
+	o) 最大值 = ${sequence.maximum()}
+	o) 元素数量 = sequence.length
+	o) 平均值 = ${sequence.average()}
+// Write your code here	
 `);
 };
 
@@ -11,8 +14,34 @@ class Sequence {
   }
 
   minimum() {
+	var min = this[0];
+    var len = this.length;
+    for (var i = 1; i < len; i++){ 
+       if (this[i] < min){ 
+           min = this[i]; 
+        } 
+    } 
+return min;
     // Write your code here
   }
-
+  maximum() {
+	var max = this[0];
+    var len = this.length;
+    for (var i = 1; i < len; i++){ 
+       if (this[i] > min){ 
+           max = this[i]; 
+        } 
+    } 
+  return max;
+  }
+  average() {
+	var sum = this[0];
+    var len = this.length;
+    var ave;
+    for (var i = 1; i < len; i++){
+    sum = sum + this[i];
+    }
+    ave = sum / len;
+    return ave;	
   // Write your code here
 }
